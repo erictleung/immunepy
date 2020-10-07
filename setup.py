0fe839947eb560c687a237046a59cbda0e7fe1a4
Eric Leung <eric@erictleung.com>
2020-10-06 20:24:13 -0700

    Create setup Python file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='immunepy',
+      version='0.0.1',
+      description=' Human immune system abstracted using Python and' + \
+              ' object-oriented programming ',
+      url='http://github.com/erictleung/immunepy/',
+      author='Eric Leung',
+      author_email='erictleung@outlook.com',
+      license='MIT',
+      packages=['immunepy'],
+      zip_safe=False)
